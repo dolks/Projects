446726b16322ee5b9ef82eeddba386f2b80f1dd8
Dylan <dylanoelkers165@gmail.com>
2017-08-28 20:45:33 -0400

    Added WIP Blackjack game

---
+++ Blackjack/test.py
@@ -0,0 +1,34 @@
+lst = [1,2,3]
+lst.append(4)
+
+print lst
+
+
+class thingy:
+
+    def __init__(self, name):
+        self.name = name
+
+
+x = thingy('hey')
+
+
+def make_bet(money):
+    while True:
+        try:
+            amt = float(raw_input('Enter bet amount: '))
+        except:
+            print 'Input is not a valid number.'
+        else:
+            if amt > money:
+                print 'You don\'t enough money for that bet'
+            else:
+                money -= amt
+                print 'You bet ' + str(amt)
+                print 'You have ' + str(money) + ' remaining.'
+                break
+
+
+make_bet(40)
+
+
