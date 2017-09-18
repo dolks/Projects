056ab780043f9bde2b72a9c733025cf0b89873ad
Dylan <dylanoelkers165@gmail.com>
2017-09-18 15:05:38 -0400

    Completed PiToNth project

--- PycharmProjects/PiToNth/pitonth.py
+++ PycharmProjects/PiToNth/pitonth.py
@@ -1,1 +1,17 @@
+import math
 
+"""
+Function that returns pi up to the nth decimal place.
+Note that it can only show up to 11 decimal places after
+the decimal point.
+"""
+
+def pitonth(n):
+    while True:
+        try:
+            num = int(n)
+        except TypeError:
+            print 'Number entered must be an integer.'
+        else:
+            result = ("%." + str(num) + "f") % math.pi
+            return float(result)
